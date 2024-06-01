4325b6465bc43604bf8f985ab702e01f5daae12c
taka.0910.takayoshi@gmail.com <fa19b046@stsis.u-hyogo.ac.jp>
2024-06-01 17:50:35 +0900

    精度を高めるためのプロンプトの工夫

--- backend/app/main.py
+++ backend/app/main.py
@@ -30,13 +30,19 @@ def read_root():
 @app.post("/upload/")
 async def upload_audio(files: List[UploadFile] = File(...)):
     results = []
+    # for file in files:
+    #     async with aiofiles.open(f'tmp/{file.filename}', 'wb') as out_file:
+    #         content = await file.read()  # Read file content
+    #         await out_file.write(content)
+    #     # 音声ファイルをテキストに変換
+    #     text = voice_to_text(f'tmp/{file.filename}')
+    #     # テキストをLLMに渡して応答を取得
+    #     response = chat(text)
+    #     results.append({"filename": file.filename, "input_text": text, "response": response})
+    # return results
     for file in files:
-        async with aiofiles.open(f'tmp/{file.filename}', 'wb') as out_file:
-            content = await file.read()  # Read file content
-            await out_file.write(content)
-        # 音声ファイルをテキストに変換
-        text = voice_to_text(f'tmp/{file.filename}')
         # テキストをLLMに渡して応答を取得
+        text = "私の強みは誰とでもうまくやっていける社交性です 私は学生時代ダンス部に所属しており1年の文化祭で文員全員でのステージを披露することが決まりました 私は幼少期からダンスを習っていた経験を変われ ダンスステージのセンターに抜粋されました 初めはプレッシャーから堂々と踊ることができなかったんですが 部長から自 分らしく踊るのが一番大切とアドバイスをいただき 肩の荷が折れた気持ちになり無事センターとしてステージを成功させることがで きました はい以上です"
         response = chat(text)
         results.append({"filename": file.filename, "input_text": text, "response": response})
     return results

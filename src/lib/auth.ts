54f213308b1ce016e1d713e53027a0bc925dbf54
mytty870 <otani11ace@icloud.com>
2024-06-03 14:55:21 +0900

    退避

---
+++ src/lib/auth.ts
@@ -0,0 +1,56 @@
+import { PrismaAdapter } from '@next-auth/prisma-adapter'
+import { NextAuthOptions } from 'next-auth'
+import GoogleProvider from 'next-auth/providers/google'
+import { prisma } from '@/lib/prisma'
+import { getServerSession as originalGetServerSession } from 'next-auth'
+
+export const authOptions: NextAuthOptions = {
+  adapter: PrismaAdapter(prisma),
+  secret: process.env.AUTH_SECRET,
+  session: {
+    strategy: 'jwt',
+  },
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID || '',
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET || '',
+    }),
+  ],
+  pages: {
+    // 認証許可が必要なページにアクセスした際のリダイレクト先
+    signIn: '/login',
+  },
+  callbacks: {
+    async jwt({ token, user }) {
+      const dbUser = await prisma.user.findFirst({
+        where: { email: token.email },
+        include: { profile: true },
+      })
+      if (!dbUser) {
+        if (user) {
+          token.id = user?.id
+        }
+        return token
+      }
+      return {
+        id: dbUser.id,
+        name: dbUser.name,
+        email: dbUser.email,
+        picture: dbUser.image,
+      }
+    },
+    async session({ session, token }) {
+      if (session.user) {
+        session.user.id = token.id as string
+        session.user.name = token.name
+        session.user.email = token.email
+        session.user.image = token.picture
+      }
+      return session
+    },
+  },
+}
+
+export const getServerSession = async () => {
+  return originalGetServerSession(authOptions)
+}

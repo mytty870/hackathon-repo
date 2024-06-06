c0d5d541b5c9d34a280228b59484d1708b93178d
taka.0910.takayoshi@gmail.com <fa19b046@stsis.u-hyogo.ac.jp>
2024-06-06 18:11:43 +0900

    インタビューページの作成

---
+++ src/app/interview/_components/StartButtonGroup.tsx
@@ -0,0 +1,11 @@
+'use client'
+import React from 'react'
+import { InterviewMainvisal } from './interviewmainvisual/interviewmainvisal'
+
+export const StartButtonGroup = () => {
+  return (
+    <>
+      <InterviewMainvisal />
+    </>
+  )
+}

1b4c992757fb47167a287ebdf0506fb563c60829
taka.0910.takayoshi@gmail.com <fa19b046@stsis.u-hyogo.ac.jp>
2024-06-07 22:00:22 +0900

    チャットボットページのデザイン修正

--- src/app/qa/_components/chatbot.tsx
+++ src/app/qa/_components/chatbot.tsx
@@ -1,11 +1,17 @@
 export default function Chatbot() {
   return (
-    <div>
-      <iframe
-        src="https://udify.app/chatbot/mrF83K66TGHwlexp"
-        className="size-full min-h-[700px]"
-        allow="microphone"
-      ></iframe>
+    <div className="mx-auto max-w-screen-lg p-4 md:p-8">
+      <h1 className="mb-8 text-center text-xl font-bold md:text-2xl">
+        就活関連チャットボット
+      </h1>
+
+      <div className="mt-12 flex items-center justify-center bg-white">
+        <iframe
+          src="https://udify.app/chatbot/mrF83K66TGHwlexp"
+          className="min-h-[400px] w-full bg-white md:min-h-[500px] md:w-[70%]"
+          allow="microphone"
+        ></iframe>
+      </div>
     </div>
   )
 }

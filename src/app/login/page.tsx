04e5a61cac49f00a732af13ffa9915650d561f08
mytty870 <otani11ace@icloud.com>
2024-06-07 14:36:04 +0900

    ログインページのデザイン修正

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,11 +12,12 @@ export default async function Page() {
     redirect('/interview')
   }
   return (
-    <div className="flex items-center justify-center">
-      <div className="flex w-full max-w-5xl flex-col rounded border bg-white p-3 shadow-md md:flex-row">
+    <div className="flex min-h-[calc(100vh-64px)] items-center justify-center bg-white md:bg-gray-100">
+      <div className="flex w-full max-w-5xl flex-col rounded-none border-none bg-white p-3 shadow-none md:flex-row md:rounded-lg md:border md:shadow-md">
         <div className="w-full p-4 md:w-1/2">
           <h1 className="mb-4 text-2xl font-bold">
-            就活支援しまっせ にようこそ！
+            <span className="text-3xl text-blue-600">就活支援しまっせ</span>{' '}
+            にようこそ！
           </h1>
           <p className="mb-6">
             就活支援しまっせは、就職活動をサポートするサービスです。
@@ -27,7 +28,7 @@ export default async function Page() {
           <p className="mb-6">さあ、就活支援しまっせと共に、充実した就活を！</p>
         </div>
         <div className="w-full p-4 md:w-1/2">
-          <h2 className="mb-4 text-xl font-semibold">ログイン</h2>
+          <h2 className="mb-6 text-xl font-semibold">ログイン</h2>
           <LoginButtonGroup />
         </div>
       </div>
